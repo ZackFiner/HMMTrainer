882789f6de0931b100726a595fbe6f51032a23eb
ZackFiner <zackary.finer@yahoo.com>
2021-04-14 18:32:46 -0700

    Update HMM.h

--- CS185C-HMM/HMM.h
+++ CS185C-HMM/HMM.h
@@ -11,8 +11,8 @@ public:
 	int* getIdealStateSequence(unsigned int* obs, unsigned int size);
 private:
 	int getStateAtT(float** gamma, unsigned int size, unsigned int t);
-	void alphaPass(unsigned int* obs, unsigned int size, float** alpha);
-	void betaPass(unsigned int* obs, unsigned int size, float** beta);
+	void alphaPass(unsigned int* obs, unsigned int size, float** alpha); // TODO: add scaling
+	void betaPass(unsigned int* obs, unsigned int size, float** beta); // TODO: add scaling
 	void calcGamma(unsigned int* obs, unsigned int size, float** alpha, float **beta, float** gamma);
 	void calcDigamma(unsigned int* obs, unsigned int size, float** alpha, float** beta, float*** digamma);
 
